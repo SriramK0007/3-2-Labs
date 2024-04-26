67734c3b85e007a22fc045f6ed34512c79e128cb
SriramK0007 <86731258+SriramK0007@users.noreply.github.com>
2024-04-26 11:27:29 +0530

    Delete IOTCC-Lab/Week7/secrets.h

--- IOTCC-Lab/Week7/secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "S21"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "12345678"	// replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID 2523858		// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "H1OSJM0XTKMIXFTD"   // replace XYZ with your channel write API Key
